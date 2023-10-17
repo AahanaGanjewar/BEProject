fc293b6e7348e7b96b34761c91a8685c96918a00
DevWhatExcites <96470846+DevWhatExcites@users.noreply.github.com>
2023-10-17 12:05:15 +0530

    Update SalesChartScreen.dart
    monthly sales chart

--- lib/widget/SalesChartScreen.dart
+++ lib/widget/SalesChartScreen.dart
@@ -12,6 +12,7 @@ class SalesChartScreen extends StatefulWidget {
 
 class _SalesChartScreenState extends State<SalesChartScreen> {
   List<SalesData> salesDataList = [];
+  DateTime selectedMonth = DateTime.now();
 
   @override
   void initState() {
@@ -32,22 +33,77 @@ class _SalesChartScreenState extends State<SalesChartScreen> {
     }
   }
 
+  void _changeMonth(int monthsToAdd) {
+    setState(() {
+      selectedMonth = DateTime(
+        selectedMonth.year,
+        selectedMonth.month + monthsToAdd,
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sales Chart'),
       ),
-      body: Center(
-        child: salesDataList.isEmpty
-            ? CircularProgressIndicator() // Display a loading indicator while data is being fetched
-            : _buildChart(),
+      body: Column(
+        children: [
+          // Month headline
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              DateFormat('MMMM yyyy').format(selectedMonth),
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          // Chart
+          Expanded(
+            child: Center(
+              child: salesDataList.isEmpty
+                  ? CircularProgressIndicator()
+                  : _buildChart(),
+            ),
+          ),
+          // Navigation buttons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  _changeMonth(-1); // Go to previous month
+                  _fetchSalesData();
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: () {
+                  _changeMonth(1); // Go to next month
+                  _fetchSalesData();
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildChart() {
     // Construct your chart here using the data in salesDataList
+    // Filter sales data for the selected month
+    List<SalesData> filteredData = salesDataList.where((data) {
+      // Convert the Firebase date string to DateTime
+      DateTime date = DateFormat("yyyy-MM-dd").parse(data.date);
+      // Check if the data falls within the selected month
+      return date.month == selectedMonth.month &&
+          date.year == selectedMonth.year;
+    }).toList();
     return LineChart(
       LineChartData(
         titlesData: FlTitlesData(
@@ -64,16 +120,15 @@ class _SalesChartScreenState extends State<SalesChartScreen> {
 
                   // Check if the index is within the bounds of your data
                   if (index >= 0 && index < salesDataList.length) {
-                    // Assuming your SalesData has a 'date' field of type DateTime
-                    final date = salesDataList[index].date;
-                    // Format the date as needed, e.g., using the intl package
-                    //return DateFormat('MMM dd').format(date as DateTime);
-                    print("karan " + date);
-                    return Text(date);
+                    // Assuming your SalesData has a 'date' field of type String
+                    final dateString = salesDataList[index].date;
+                    // Parse the string to a DateTime object
+                    final date = DateTime.parse(dateString);
+                    // Format the date as needed
+                    return Text(DateFormat('MMM dd').format(date));
                   }
 
-                  //return ''; // Return an empty string for invalid indices
-                  return Text('');
+                  return Text(''); // Return an empty string for invalid indices
                 },
               ),
             )),
@@ -81,7 +136,7 @@ class _SalesChartScreenState extends State<SalesChartScreen> {
         // Example: Add sales data from salesDataList to spots in LineChartBarData
         lineBarsData: [
           LineChartBarData(
-            spots: salesDataList.asMap().entries.map((entry) {
+            spots: filteredData.asMap().entries.map((entry) {
               return FlSpot(entry.key.toDouble(), entry.value.amount);
             }).toList(),
             isCurved: true,
